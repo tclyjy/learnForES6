e2d2c12dde2a659b5e460b3d05af91c9cd35bb12
元家怿 <yuanjiayi@yuanjiayideMacBook-Pro.local>
2016-12-20 19:40:40 +0800

    function/01.js

--- Function/01.js
+++ Function/01.js
@@ -159,3 +159,90 @@ function foo(x = 5, y = 6) {
 }
 
 foo(undefined, null) // 5 null
+
+// 函数的length属性
+
+//指定了默认值以后，函数的length属性，将返回没有指定默认值的参数个数。也就是说，指定了默认值后，length属性将失真。
+(function (a) {}).length // 1
+  (function (a = 5) {}).length // 0
+  (function (a, b, c = 5) {}).length // 2
+  //上面代码中，length属性的返回值，等于函数的参数个数减去指定了默认值的参数个数。比如，上面最后一个函数，定义了3个参数，其中有一个参数c指定了默认值，因此length属性等于3减去1，最后得到2。
+
+//这是因为length属性的含义是，该函数预期传入的参数个数。某个参数指定默认值以后，预期传入的参数个数就不包括这个参数了。同理，rest参数也不会计入length属性。
+
+  (function (...args) {}).length // 0
+  //如果设置了默认值的参数不是尾参数，那么length属性也不再计入后面的参数了。
+
+  (function (a = 0, b, c) {}).length // 0
+  (function (a, b = 1, c) {}).length // 1
+
+//作用域  (如果参数默认值是一个变量，则该变量所处的作用域，与其他变量的作用域规则是一样的，即先是当前函数的作用域，然后才是全局作用域。)
+var x = 1;
+
+function f(x, y = x) {
+  console.log(y);
+}
+
+f(2) // 2
+  /*===========================================*/
+var x = 1;
+
+function f(y = x) {
+  let x = 2;
+  console.log(y);
+}
+f() // 1
+  /*===========================================*/
+function f(y = x) {
+  let x = 2;
+  console.log(y);
+}
+
+f() // ReferenceError: x is not defined
+  /*===========================================*/
+var x = 1;
+
+function foo(x, y = function () {
+  x = 2;
+}) {
+  var x = 3;
+  y();
+  console.log(x);
+}
+
+foo() // 3
+
+//函数一开始 参数值x=undefined , 之后赋值 x=2 ，进入函数foo后又有局部变量 x=3 所以输出3
+/*===========================================*/
+var x = 1;
+
+function foo(x, y = function () {
+  x = 2;
+}) {
+  x = 3;
+  y();
+  console.log(x);
+}
+
+foo() // 2
+  //函数一开始 参数值x=undefined , 之后赋值 x=2， 因为作用域关系有局部参数值可用，不取全局X的值
+
+//函数默认值的应用
+//利用参数默认值，可以指定某一个参数不得省略，如果省略就抛出一个错误。
+function throwIfMissing() {
+  throw new Error('Missing parameter');
+}
+
+function foo(mustBeProvided = throwIfMissing()) {
+  return mustBeProvided;
+}
+
+foo()
+  // Error: Missing parameter
+foo(12)
+  //12
+foo("Hello");
+//Hello
+
+//另外，可以将参数默认值设为undefined，表明这个参数是可以省略的。
+function foo(optional = undefined) {}
